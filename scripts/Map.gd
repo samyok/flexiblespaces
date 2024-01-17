06c89cf32ac78707bf1b168b5ffabbdba275663b
jklpo360 <suduerik@gmail.com>
2024-01-17 08:45:54 -0600

    progress

--- scripts/Map.gd
+++ scripts/Map.gd
@@ -1,180 +1,215 @@
 extends Node3D
 
-var in_hand = false
-var dominant_hand = "left" # "left" or "right"
 var node_currently_tracking # updates the position and rotation of the Map to this node's transform in _process
-var lerp_start_node 
-var lerp_end_node 
-var lerp_from_hip = false
-var lerp_to_hip = false
-var lerp_in_progress = false # true or false
-var lerp_speed = 1.0 # time (in seconds) for the map to lerp between points
-var current_alpha = 0.0 # progress from 0.0-1.0 of current lerp
+var camera
 var left_controller
-var right_controller 
-var left_hip
-var right_hip
-var active_hand_grip_down = false
-var last_movement_direction
+var right_controller
+var grip_pressed = false
+var last_cursor_position = Vector2(0.0, 0.0)
 var input_vector = Vector2(0.0, 0.0)
 var scaling_deadzone = .2
-var scaling_speed = Vector2(1.0, 1.0)
-var max_scale = Vector2(4.0, 4.0)
+var scaling_speed = 1.0
+var max_scale = 4.0
+var cursor
+var cursor_block
+var active
+var path_multi_mesh
+var corner_multi_mesh
+var rooms # Room array (Vector2 -> Room node dictionary)
+var paths # Path array (Vector2 -> instance id)
+var corners # Path corner array (Vector2 -> instance id)
+var adjacencies # Vector3 array (path index, room id1, room id 2)
+var dragging_room # null or the room being currently dragged
+var dragging_path # null or the path being currently dragged
+var path_ghost_transparency = .4 # Transparency (0 to 1) of the pending path
+var valid_path_start
+var current_path_origin
+var current_path_last_block
+var double_click_timer = 0
+var double_click_timing = false
+var double_click_start_block
+var double_click_window = .4 # Maximum time (in seconds) between first and second click when double clicking
+var room_queue = []
+var rooms_queued = false
+var room_index = 0
 
 
-signal hip_hitbox_show # TODO: link to both hip hitboxes
-signal hip_hitbox_hide # TODO: link to both hip hitboxes
-signal size_changed # TODO: link to the drawing pane if necessary
+
+signal size_changed # TODO: add parameter and link to the drawing pane if necessary
 signal pause_stick_movement # TODO: link to locomotion.gd
 signal resume_stick_movement # TODO: link to locomotion.gd
+signal cursor_position_changed # TODO: add parameter and link to the drawing pane
+signal cursor_click_started  # TODO: link to the drawing pane
+signal cursor_click_ended # TODO: link to the drawing pane
+signal cursor_double_click # TODO: link to the drawing pane
+signal show_screen # TODO: link to the drawing pane
+signal hide_screen # TODO: link to the drawing pane
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	left_controller = %LeftController
 	right_controller = %RightController
-	left_hip = %LeftHip
-	right_hip = %RightHip
-	node_currently_tracking = right_hip
+	camera = %XRCamera3D
+	node_currently_tracking = null
+	path_multi_mesh = find_child("PathMultiMesh")
+	corner_multi_mesh = find_child("CornerMultiMesh")
+	rooms = [find_child("Room1"), find_child("Room2"), find_child("Room3"), find_child("Room4"), find_child("Room5")]
+	paths = []
+	cursor = find_child("") # TODO: instanciate
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	# (Movement)
-	# If lerping:
-	#   Lerp between positions of lerp_start_node and lerp_end_node
-	# Else:
+	# (Transform)
+	# If grip_pressed:
 	#   Position update to node_currently_tracking
-	# (Rotation)
-	# If lerping: 
-	#   Lerp between lookat(XR_Camera) and hip_direction
-	# Elif in a hand:
 	# 	Rotation update to look at XR_Camera
-	# Else:
-	# 	Rotation update matches orthogonal to last_movement_direction and Up vector
-  # (Scaling)
-	# If active_hand_grip_down and input is recieved from active controller's stick:
-	#   Scale the map by the input_vector multiplied by the scaling speed vector
-	if lerp_in_progress:
-		current_alpha += lerp_speed * delta
-		if current_alpha > 1:
-			current_alpha = 1
-		self.global_position = lerp_start_node.global_position.lerp(lerp_end_node.global_position, current_alpha)
-		if lerp_from_hip:
-			var hip_rotation = last_movement_direction * PI * 2
-			self.rotation = hip_rotation.lerp(lerp_end_node.rotation, current_alpha)
-		elif lerp_to_hip:
-			var hip_rotation = last_movement_direction * PI * 2
-			self.rotation = lerp_start_node.rotation.lerp(hip_rotation, current_alpha)
-		else:
-			self.rotation = lerp_start_node.rotation.lerp(lerp_end_node.rotation, current_alpha)
-		if current_alpha == 1:
-			clean_up_lerp()
-	else:
+	#   If grip_pressed and input is recieved from active controller's stick:
+	#     Scale the map by the input_vector multiplied by the scaling speed vector
+	#   Move the cursor
+	#   Time the double click if we are currently doing so
+	#   If dragging
+	#     Float the block the user is dragging above the others and create a transparent clone where it would go when released
+	#     Draggin ghost
+	#   Elif pathing
+	#     If just moved to a new block, update the path
+	#     Pathing ghost
+	#   Else (not pathing)
+	#     Room queued ghost
+	#     Start path ghost
+
+
+	if grip_pressed:
 		self.global_position = node_currently_tracking.global_position
-		if not in_hand:
-			self.rotation = last_movement_direction * PI * 2
+		self.rotation = node_currently_tracking.rotation
+		if grip_pressed and input_vector.length() > scaling_deadzone and (self.scale.x < self.max_scale.x and self.scale.y < self.max_):
+			self.scale = self.scale + Vector3(scaling_speed.x * input_vector.x, scaling_speed.y * input_vector.y, 0.0)
+			size_changed.emit()
+
+		# Right controller cursor update
+		if node_currently_tracking == left_controller:
+			var drawing_plane = Plane((left_controller.position - camera.position).normalized(), left_controller.position)
+			cursor.position = drawing_plane.project(right_controller.position)
+			cursor.find_child("Laser").global_scale = cursor_position.distance_to(right_controller)
+			block_position  = (cursor.position - left_controller.position).ceil()
+
+		# Left controller cursor update
 		else:
-			self.rotation = node_currently_tracking.rotation
-	if active_hand_grip_down and input_vector.length() > 0 and (self.scale.x < self.max_scale.x and self.scale.y < self.max_):
-		self.scale = self.scale + Vector3(scaling_speed.x * input_vector.x, scaling_speed.y * input_vector.y, 0.0)
-		size_changed.emit()
+			var drawing_plane = Plane((right_controller.position - camera.position).normalized(), right_controller.position)
+			cursor.position = drawing_plane.project(left_controller.position)
+			cursor.find_child("Laser").global_scale = cursor_position.distance_to(left_controller)
+			block_position  = (cursor.position - right_controller.position).ceil()
 
-func initiate_lerp(to):
-	if not lerp_in_progress:
-		if not in_hand:
-			lerp_from_hip = true
-		if to == "left":
-			lerp_in_progress = true
-			lerp_start_node = node_currently_tracking
-			lerp_end_node = left_controller
-			in_hand = true
-		elif to == "right":
-			lerp_in_progress = true
-			lerp_start_node = node_currently_tracking
-			lerp_end_node = right_controller
-			in_hand = true
-		elif to == "left_hip":
-			lerp_in_progress = true
-			lerp_start_node = node_currently_tracking
-			lerp_end_node = left_hip
-			in_hand = false
-			lerp_to_hip = true
-		elif to == "right_hip":
-			lerp_in_progress = true
-			lerp_start_node = node_currently_tracking
-			lerp_end_node = right_hip
-			in_hand = false
-			lerp_to_hip = true
+		# Double click timing
+		if double_click_timing:
+			double_click_timer += delta
+			if double_click_timer > double_click_window:
+				double_click_timing = false
+		
+		# Dragging
+		if 
+		
+		# Pathing
 
-func clean_up_lerp():
-	current_alpha = 0
-	node_currently_tracking = lerp_end_node
-	lerp_in_progress = false
-	lerp_start_node = null
-	lerp_end_node = null
-	lerp_from_hip = false
-	lerp_to_hip = false
+		# Dragging or room queued ghost
 
+		# Path start or pathing ghost
+
+# Attempts to place a room in the location from the queue
+func place_room():
+
+# Attempts to start dragging the current block
+func start_drag():
+
+# Attempts to start pathing at the current block
+func start_path():
+	var 
+	if valid_path_start:
+		current_path_last_block = cursor_block
+		current_path_origin = cursor_block
+
+# Adjusts current pathing with new information 
+
+# Cleans up room or path dragging
+func dragging_clean_up():
+	# Invalid call case
+	if dragging_path == null && dragging_room == null:
+	   return
+	# Path case
+	elif dragging_room == null:
+		# If current cursor block is next to a room then 
+	# Room case
+	else:
+
+# TODO: attach signal from left controller
 func _on_left_controller_button_pressed(name):
-	if name == "grip_click":
-		active_hand_grip_down = true
-		if node_currently_tracking != left_controller:
-			initiate_lerp("left")
+	if name == "grip_click" and grip_pressed != true:
+		grip_pressed = true
+		self.show()
+		node_currently_tracking = left_controller
+		pause_stick_movement.emit()
+	elif name = "trigger_click":
+		if double_click_timing and double_click_start_block == cursor_block:
+			delete_section()
 		else:
-			pause_stick_movement.emit()
-			hip_hitbox_show.emit()
+			double_click_timer = 0.0
+			double_click_timing = true
+			double_click_start_block = cursor_block
+			# place room
+			# start path
+			# start drag
+		
+		
 
+# TODO: attach signal from right controller
 func _on_right_controller_button_pressed(name):
-	if name == "grip_click":
-		active_hand_grip_down = true
-		if node_currently_tracking != right_controller:
-			initiate_lerp("right")
-		else:
-			pause_stick_movement.emit()
-			hip_hitbox_show.emit()
+	if name == "grip_click" and grip_pressed != true:
+		grip_pressed = true
+		self.show()
+		node_currently_tracking = right_controller
+		pause_stick_movement.emit()
+	elif name = "trigger_click":
 
 
 # TODO: attach signal from left controller
 func _on_left_controller_button_released(name):
 	if name == "grip_click":
-		active_hand_grip_down = false
-		hip_hitbox_hide.emit()
+		grip_pressed = false
+		self.hide()
 		resume_stick_movement.emit()
+	elif name = "trigger_click":
+		
 
 # TODO: attach signal from right controller
 func _on_right_controller_button_released(name):
 	if name == "grip_click":
-		active_hand_grip_down = false
-		hip_hitbox_hide.emit()
+		grip_pressed = false
+		self.hide()
 		resume_stick_movement.emit()
-
-# TODO: attach signal from locomation
-func _on_movement_direction_changed(new_direction):
-	last_movement_direction = new_direction
-
-# TODO: attach signal from left hip hitbox
-func _on_left_hip_area_3d_area_entered(area):
-	if area == self.find_child("MapHitbox"):
-		if active_hand_grip_down:
-			input_vector = Vector2(0.0, 0.0)
-			initiate_lerp("left_hip")
-
-# TODO attach signal from right hip hitbox
-func _on_right_hip_area_3d_area_entered(area):
-	if area == self.find_child("MapHitbox"):
-		if active_hand_grip_down:
-			input_vector = Vector2(0.0, 0.0)
-			initiate_lerp("right_hip")
+	elif name = "trigger_click":
+		
 
 # TODO: attach signal from left controller
 func _on_left_controller_input_vector_2_changed(name, value):
-	if name == "primary" and in_hand == true and node_currently_tracking.ID == left_controller.ID and value.length() > scaling_deadzone:
-		input_vector = value
+	if name == "primary":
+		if grip_pressed == true and node_currently_tracking == left_controller and value.length() > scaling_deadzone:
+			input_vector = value
+		else:
+			input_vector = Vector2(0, 0)
 
 # TODO: attach signal from right controller
 func _on_right_controller_input_vector_2_changed(name, value):
-	if name == "primary" and in_hand == true and node_currently_tracking.ID == right_controller.ID and value.length() > scaling_deadzone:
-		input_vector = value
-			
+	if name == "primary":
+		if grip_pressed == true and node_currently_tracking == right_controller and value.length() > scaling_deadzone:
+			input_vector = value
+		else:
+			input_vector = Vector2(0, 0)
+
+# TODO: attach signal from somewhere
+func _on_room_explored():
+	if room_index != 5:
+		rooms_queued = true
+		room_queue.push_front(rooms[room_index])
+		room_index += 1
