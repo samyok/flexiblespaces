c5c3392064f5641ab801edb71bec58091623714d
jklpo360 <suduerik@gmail.com>
2024-02-05 14:04:49 -0600

    demo for ESR

--- scripts/FlexibleRoom.gd
+++ scripts/FlexibleRoom.gd
@@ -13,13 +13,27 @@ var current_room = 0
 						# 4: E
 						# 5: F
 						# 6: G
+var current_rotation = 0 
 var last_room
 var next_room
 var last_door
 var next_door
 var doors # Door pointers
+var rooms_parent # Node to hide all rooms
 var rooms # Wall pointers
-var signs # TODO: use these
+var signs # Left sign pointers
+var door_guides # Door guide pointers
+var floor_guides # Floor guide pointers (0: turn left, 1: straight, 2: turn right)
+var signs_shown = []
+
+var dfs = true # True for Depth-first search, False for Breadth-first search
+var dfs_room_order =        [0, 1, 3, 1, 5, 4, 0, 2, 6]
+var dfs_door_guide_order =  [0, 0, -1, 0, 1, 3, 2, 3, -1]
+var dfs_floor_guide_order = [3, 2, -1, 0, 1, 2, 2, 1, -1]
+var bfs_room_order = [0, 1, 2, 4, 5, 1, 3, 1, 0, 2, 6]
+var bfs_door_guide_order = []
+var bfs_floor_guide_order = []
+var current_search_index = 0
  
 # Room A has B north, E to the east, C to the south, and _ to the west
 # Room B has D north, F to the east, _ to the south, and A to the west
@@ -36,9 +50,18 @@ signal exited_hallway
 signal explored_room(room)
 
 func _ready():
-	doors = [%DoorNorthArea, %DoorEastArea, %DoorSouthArea, %DoorWestArea]
-	rooms = [self.find_child("Room1"), self.find_child("Room2"), self.find_child("Room3"), self.find_child("Room4"), self.find_child("Room5"), self.find_child("Room6"), self.find_child("Room7")]
-	
+	doors = self.find_child("Doors").find_children("Door*")
+	rooms_parent = self.find_child("Rooms")
+	rooms = rooms_parent.find_children("Room?")
+	signs = self.find_child("Signs").find_children("Sign?")
+	door_guides = self.find_child("Guides").find_children("DoorGuide*")
+	floor_guides = self.find_child("Guides").find_children("FloorGuide*")
+	if dfs:
+		door_guides[dfs_door_guide_order[0]].show()
+		floor_guides[dfs_floor_guide_order[0]].show()
+	else:	
+		door_guides[bfs_door_guide_order[0]].show()
+		floor_guides[bfs_floor_guide_order[0]].show()
 
 func _process(delta):
 	pass
@@ -46,20 +69,43 @@ func _process(delta):
 func change_context(door):
 	# If currently in a room
 	if current_room > -1:
-		rooms[current_room].hide()
-		self.hide()
-		last_room = current_room
-		next_room = adjacencies[current_room][door]
-		last_door = door
-		for i in 4:
-			doors[i].hide()
-		for i in 4:
-			if adjacencies[next_room][i] == last_room:
-				next_door = i
-		doors[last_door].show()
-		doors[next_door].show()
-		entered_hallway.emit(last_door, next_door)
-		current_room = -1
+		if adjacencies[current_room][door] != -2:
+			if dfs:
+				if current_room == dfs_room_order[current_search_index]:
+					if dfs_door_guide_order[current_search_index] != -1:
+						door_guides[dfs_door_guide_order[current_search_index]].hide()
+					if dfs_floor_guide_order[current_search_index] != -1:
+						floor_guides[dfs_floor_guide_order[current_search_index]].hide()
+			else:
+				if current_room == bfs_room_order[current_search_index]:
+					if bfs_door_guide_order[current_search_index] != -1:
+						door_guides[bfs_door_guide_order[current_search_index]].hide()
+					if bfs_floor_guide_order[current_search_index] != -1:
+						floor_guides[bfs_floor_guide_order[current_search_index]].hide()
+			rooms[current_room].hide()
+			rooms_parent.hide()
+			last_room = current_room
+			next_room = adjacencies[current_room][door]
+			if dfs:
+				if next_room == dfs_room_order[current_search_index+1]:
+					current_search_index += 1
+			else:
+				if next_room == bfs_room_order[current_search_index+1]:
+					current_search_index += 1
+			last_door = door
+			var size = signs_shown.size()
+			for i in size:
+				var sign = signs_shown.pop_front()
+				sign.hide()
+			for i in 4:
+				doors[i].hide()
+			for i in 4:
+				if adjacencies[next_room][i] == last_room:
+					next_door = i
+			doors[last_door].show()
+			doors[next_door].show()
+			entered_hallway.emit(last_door, next_door)
+			current_room = -1
 	# If in a hallway
 	else:
 		# Came back the same way
@@ -67,13 +113,43 @@ func change_context(door):
 			current_room = last_room
 			swap_room()
 			exited_hallway.emit()
-			self.show()
+			if dfs:
+				if next_room == dfs_room_order[current_search_index]:
+					current_search_index -= 1
+				if current_room == dfs_room_order[current_search_index]:
+					if dfs_door_guide_order[current_search_index] != -1:
+						door_guides[dfs_door_guide_order[current_search_index]].show()
+					if dfs_floor_guide_order[current_search_index] != -1:
+						floor_guides[dfs_floor_guide_order[current_search_index]].show()
+			else:
+				if next_room == bfs_room_order[current_search_index]:
+					current_search_index -= 1
+				if current_room == bfs_room_order[current_search_index]:
+					if bfs_door_guide_order[current_search_index] != -1:
+						door_guides[bfs_door_guide_order[current_search_index]].show()
+					if bfs_floor_guide_order[current_search_index] != -1:
+						floor_guides[bfs_floor_guide_order[current_search_index]].show()
+			rooms_parent.show()
 		# Went to the other door
 		elif next_door == door:
 			current_room = next_room
 			swap_room()
 			exited_hallway.emit()
-			self.show()
+			if dfs:
+				if current_room == dfs_room_order[current_search_index]:
+					if dfs_door_guide_order[current_search_index] != -1:
+						door_guides[dfs_door_guide_order[current_search_index]].show()
+					if dfs_floor_guide_order[current_search_index] != -1:
+						floor_guides[dfs_floor_guide_order[current_search_index]].rotation.y = -PI*door/2
+						floor_guides[dfs_floor_guide_order[current_search_index]].show()
+			else:
+				if current_room == bfs_room_order[current_search_index]:
+					if bfs_door_guide_order[current_search_index] != -1:
+						door_guides[bfs_door_guide_order[current_search_index]].show()
+					if bfs_floor_guide_order[current_search_index] != -1:
+						floor_guides[bfs_floor_guide_order[current_search_index]].rotation.y = -PI*door/2
+						floor_guides[bfs_floor_guide_order[current_search_index]].show()
+			rooms_parent.show()
 
 func swap_room():
 	
@@ -84,6 +160,15 @@ func swap_room():
 			doors[i].hide()
 		else:
 			doors[i].show()
+			if explored[door]:
+				var sign = signs[door]
+				# Rotate signs to be at current door
+				sign.rotation.y = -PI*i/2
+				# Show the signs
+				sign.show()
+				# Mark them to be hidden when next entering a hallway
+				signs_shown.push_front(sign)
+				
 	rooms[current_room].show()
 	if not explored[current_room]:
 		explored[current_room] = true
@@ -93,3 +178,4 @@ func _on_door_entered(area):
 	for i in 4:
 		if area == doors[i]:
 			change_context(i)
+
